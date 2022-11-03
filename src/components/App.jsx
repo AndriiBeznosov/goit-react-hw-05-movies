80f8ac5ced5657e167798b1d61fd372362249d1d
Andrii Beznosov <andrey301288@gmail.com>
2022-11-03 19:00:17 +0200

    +

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,12 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  background-color: ${p => p.theme.colors.backgroundColor};
+  color: ${p => p.theme.colors.primaryText};
+  height: 100vh;
+  font-size: 16px;
+`;
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  return <Container>React homework template</Container>;
 };
---
+++ src/utils/Thema.js
@@ -0,0 +1,15 @@
+export const theme = {
+  colors: {
+    white: '#fff',
+    black: '#000',
+    primaryText: '#ffffff',
+    secondaryText: '#cfcbc8',
+    backgroundColor: '#013f4e',
+    backgroundSecondary: '#00718f',
+    accent: '#0ba5be',
+    secondaryColor: '#576319',
+  },
+  space: value => `${4 * value}px`,
+  transition: '250ms cubic-bezier(0.4, 0, 0.2, 1)',
+  boxShadow: '0px 5px 10px 2px rgba(255, 255, 255, 0.3);',
+};
